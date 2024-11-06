dbb05bd7f7f12c0b8d5d44506f9f7609306c46cb
Oleg Ivanov <o.ivanov@innopolis.ru>
2024-11-07 01:13:57 +0300

    Add env activating step

--- setup.sh
+++ setup.sh
@@ -15,26 +15,31 @@ handle_error() {
 # Check if the environment already exists
 if conda env list | grep -q "^$ENV_NAME\s"; then
     echo "Environment $ENV_NAME already exists. Activating it..."
+
+    # Activate the environment
+    echo "Activating the environment..."
+    conda activate $ENV_NAME || handle_error "Failed to activate environment $ENV_NAME."
+
 else
     # Create the Conda environment
     echo "Creating Conda environment..."
     conda create -n $ENV_NAME python=$PYTHON_VERSION -y || handle_error "Failed to create environment $ENV_NAME." && conda activate $ENV_NAME
+
+    # Activate the environment
+    echo "Activating the environment..."
+    conda activate $ENV_NAME || handle_error "Failed to activate environment $ENV_NAME."
+
+    echo "Installing requirements..."
+    pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+    pip install -r requirements.txt || handle_error "Failed to install requirements\n Try to install manually."
+
 fi
 
-# Activate the environment
-echo "Activating the environment..."
-conda activate $ENV_NAME || handle_error "Failed to activate environment $ENV_NAME."
-
-echo "Installing requirements..."
-
-echo "Installing torch and torchvision..."
-conda install -y pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
-
-pip install -r requirements.txt || handle_error "Failed to install requirements\n Try to install manually."
+source clearml.sh
 
 # Verify the environment was created and activated
 if [[ $CONDA_DEFAULT_ENV == $ENV_NAME ]]; then
-    echo "Environment $ENV_NAME has been successfully created and activated."
+    echo "Environment $ENV_NAME has been successfully activated."
 else
     handle_error "Failed to activate environment $ENV_NAME."
 fi

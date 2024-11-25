14c5f1249ae0bbb34e1303f389a9a19a4f708a7b
Oleg Ivanov <o.ivanov@innopolis.ru>
2024-11-25 23:31:18 +0300

    Add class distribution visualization

--- uncertainty_skin/src/train.py
+++ uncertainty_skin/src/train.py
@@ -3,7 +3,7 @@ from hydra.utils import get_original_cwd
 from omegaconf import DictConfig
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
-from src.data.datamodule import ISICDataModule
+from src.data.datamodule import ISICDataModule, ChestDataModule
 from src.models.cnn_model import CNN
 from src.models.timm_model import TimmModel
 from src.utils.clearml_logger import ClearMLLogger
@@ -11,6 +11,9 @@ from src.utils.utils import set_seed
 import torch
 import os
 import uuid
+
+from src.utils.metrics import plot_pca
+from src.utils.utils import extract_features
 
 def create_model(config):
     if config.model.name == 'CNN':
@@ -30,15 +33,23 @@ def train(config: DictConfig,
 
     
     config.dataset.seed = seed
+    
+    model_slug = f'{config.model.name}_{seed}_{config.model.loss_fun}_{unique_id}'
 
     os.makedirs('checkpoints', exist_ok=True)
 
     data_module = ISICDataModule(config.dataset)
     model = create_model(config)
+    
+    # Extract embeddings before training
+    
+    data_module.setup()
+    
+    features_before, labels_before = extract_features(model, data_module.train_dataloader())
 
     checkpoint_callback = ModelCheckpoint(
         save_weights_only=True, mode="min", monitor="val_loss", dirpath='checkpoints/',
-        filename=f'{config.model.name}_{seed}_{config.model.loss_fun}_{unique_id}_' + '{epoch}'
+        filename=f'{model_slug}_' + '{epoch}'
     )
 
     callbacks = [
@@ -65,6 +76,15 @@ def train(config: DictConfig,
     os.makedirs(os.path.join(original_dir, 'checkpoints'), exist_ok=True)
     os.system(f"cp {trainer.checkpoint_callback.best_model_path} {os.path.join(original_dir, 'checkpoints')}")
 
+    features_after, labels_after = extract_features(model, data_module.train_dataloader())
+    
+    print(features_after)
+    
+    try:
+        plot_pca(features_before, labels_before, f'Class Distribution Before Training', model_slug)
+        plot_pca(features_after, labels_after, f'Class Distribution After Training', model_slug)    
+    except: 
+        print('Cannot extract features and plot embeddings')
 
     return trainer.checkpoint_callback.best_model_path
 

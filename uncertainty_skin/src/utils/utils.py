151860860ce382c9c6602e8eef0b5c7b50fb50ec
Oleg Ivanov <o.ivanov@innopolis.ru>
2024-11-25 23:32:55 +0300

    Add extract features function for class visualization

--- uncertainty_skin/src/utils/utils.py
+++ uncertainty_skin/src/utils/utils.py
@@ -2,8 +2,23 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 def set_seed(seed):
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
+    
 
+def extract_features(model, dataloader):
+    features = []
+    labels = []
+    model.eval()
+    with torch.no_grad():
+        for x, y in dataloader:
+            embeddings = model.intermediate_forward(x)
+            features.append(embeddings.cpu().numpy())
+            labels.append(y.cpu().numpy())
+    features = np.concatenate(features, axis=0)
+    labels = np.concatenate(labels, axis=0)
+    return features, labels

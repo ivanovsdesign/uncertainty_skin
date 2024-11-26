2a6399b9af593b55a6bb682daec6e82eff0d0190
Oleg Ivanov <o.ivanov@innopolis.ru>
2024-11-26 22:34:02 +0300

    Add chest dataset

--- uncertainty_skin/src/data/datamodule.py
+++ uncertainty_skin/src/data/datamodule.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
-from .dataset import CustomImageDataset
+from .dataset import CustomImageDataset, ClassDataset
 
 from torchvision import transforms
 
@@ -47,6 +47,58 @@ class ISICDataModule(pl.LightningDataModule):
         self.val_dataset = CustomImageDataset(val_df, self.config.path, self.transform['test'])
         self.test_dataset = CustomImageDataset(test_df, self.config.path, self.transform['test'])
         self.test_tta_dataset = CustomImageDataset(test_df, self.config.path, self.transform['test_tta'])
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.config.batch_size, num_workers=self.config.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.config.batch_size, num_workers=self.config.num_workers)
+
+    def test_dataloader(self):
+        if self.config.num_tta > 1:
+            return DataLoader(self.test_tta_dataset, batch_size=self.config.batch_size, num_workers=self.config.num_workers)
+        else:
+            return DataLoader(self.test_dataset, batch_size=self.config.batch_size, num_workers=self.config.num_workers)
+        
+class ChestDataModule(pl.LightningDataModule):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.train_dataframe = pd.read_csv(config.train_path)
+        self.val_dataframe = pd.read_csv(config.val_path)
+        self.test_dataframe = pd.read_csv(config.test_path)
+        
+        self.image_path = config.image_path
+        self.image_size = config.img_size
+        
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),  # Convert image to tensor
+            #v2.Lambda(lambda x: x / 255.0),  # Divide by 255
+            transforms.Resize((config.img_size, config.img_size)),
+            transforms.Grayscale(num_output_channels=3),
+            #transforms.RandomResizedCrop(config.img_size, scale=(0.85, 1), antialias=True),
+        ])
+        
+        self.test_transform = transforms.Compose([
+            transforms.ToTensor(),  # Convert image to tensor
+            #v2.Lambda(lambda x: x / 255.0),  # Divide by 255
+            transforms.Resize((config.img_size, config.img_size)),
+            transforms.Grayscale(num_output_channels=3)
+        ])
+        
+        self.tta_transform = transforms.Compose([
+            transforms.ToTensor(),  # Convert image to tensor
+            #v2.Lambda(lambda x: x / 255.0),  # Divide by 255
+            transforms.Resize((config.img_size, config.img_size)),
+            transforms.Grayscale(num_output_channels=3),
+            transforms.RandomResizedCrop(config.img_size, scale=(0.85, 1), antialias=True),
+        ])
+
+    def setup(self, stage=None):
+        self.train_dataset = ClassDataset(self.train_dataframe, self.image_path, self.transform)
+        self.val_dataset = ClassDataset(self.val_dataframe, self.image_path, self.transform)
+        self.test_dataset = ClassDataset(self.test_dataframe, self.image_path, self.test_transform)
+        self.test_tta_dataset = ClassDataset(self.test_dataframe, self.image_path, self.tta_transform)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.config.batch_size, num_workers=self.config.num_workers)
--- uncertainty_skin/src/data/dataset.py
+++ uncertainty_skin/src/data/dataset.py
@@ -3,6 +3,9 @@ from PIL import Image
 import cv2
 import torch
 import numpy as np
+from torchvision import transforms
+from torchvision.transforms import v2
+from tqdm import tqdm
 
 class CustomImageDataset(Dataset):
     def __init__(self, dataframe, images_path, transform=None):
@@ -21,18 +24,13 @@ class CustomImageDataset(Dataset):
         return image, int(self.dataframe.loc[self.dataframe.index[idx], 'target'])
     
 class ClassDataset(Dataset):
-    def __init__(self, dataframe, images_path, image_size, preload_transform=None, transform=None):
+    def __init__(self, dataframe, images_path, transform=None):
         self.dataframe = dataframe.dropna().reset_index()
-        self.preload_transform = preload_transform
+
         self.transform = transform
 
-        self.images = torch.zeros(len(dataframe), 3, image_size, image_size).float()
-        
-        for idx in range(len(self.dataframe)):
-            image_path = self.dataframe.loc[idx, 'index']
-            image = cv2.imread(f'{images_path}/{image_path}', cv2.IMREAD_GRAYSCALE)
-            if self.preload_transform:
-                self.images[idx] = self.preload_transform(torch.tensor(image.astype(np.float32) / 255).unsqueeze(0))
+        self.images_path = images_path
+    
 
     def set_transform(self, transform):
         self.transform = transform
@@ -41,8 +39,8 @@ class ClassDataset(Dataset):
         return len(self.dataframe)
 
     def __getitem__(self, idx):
+        image_path = self.dataframe.iloc[idx, 1]
+        image = Image.open(f'{self.images_path}/{image_path}').convert('RGB')
         if self.transform:
-            image = self.transform(self.images[idx])
-        else:
-            image = self.images[idx]
+            image = self.transform(image)
         return image, int(self.dataframe.loc[idx, 'CTI_label'])
